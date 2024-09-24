fa441b3777735b8ca4d0faa91d0e2267e5c96cef
Stephen Nneji <steve.nneji@gmail.com>
2024-09-24 11:51:53 +0100

    Update distribution again

--- installer/macos/make.sh
+++ installer/macos/make.sh
@@ -14,11 +14,11 @@ codesign -v --deep --force --options=runtime --entitlements ./entitlements.plist
 codesign -v --deep --force --options=runtime --entitlements ./entitlements.plist --sign ${DEV_TEAM_ID} --timestamp ${SSCANSS_PATH}
 
 # Build Pkg
+sed -e 's/@VERSION@/${Ver}/g' distribution.xml.in > distribution.xml
 pkgbuild --root ${EDITOR_PATH} --identifier com.sscanss2.editor.pkg --version ${VER} --install-location "/Applications/sscanss-editor.app" editor.pkg
 pkgbuild --root ${SSCANSS_PATH} --identifier com.sscanss2.sscanss.pkg --version ${VER} --install-location "/Applications/sscanss.app" sscanss.pkg
 productbuild --sign ${DEV_TEAM_ID} --timestamp --distribution distribution.xml --resources . sscanss2.pkg
 
 # Notarise and staple
-#echo -n $API_CONNECT_KEY | base64 -d -o  ./auth_key.p8
 xcrun notarytool submit --issuer ${API_CONNECT_ISSUER} --key-id ${API_CONNECT_KEY_ID} --key ./auth_key.p8 --wait sscanss2.pkg 
 xcrun stapler staple sscanss2.pkg
